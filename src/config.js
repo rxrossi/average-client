9c2cdde7d2d624fa2428996aa08bc305ea976063
rxrossi <rx.rossi@gmail.com>
2018-05-19 14:41:28 -0300

    Updated readme

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // require("dotenv").config();
 
 module.exports = {
-  // API_ADDRESS: process.env.REACT_APP_API_ADDRESS
+  // API_ADDRESS: process.env.REACT_APP_API_ADDRESS, not chosen because it conflicts with storybook
   API_ADDRESS: 'http://localhost:5001'
 }
